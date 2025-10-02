a657f21671bb71e9b6c34a05ccae28bf37a222c3
BorisMejia <borisfernandez916@gmail.com>
2025-10-02 17:41:40 -0500

    feat: implementacion del modelo franquicia y sus puertos

---
+++ domain/model/src/main/java/co/com/franquicias/model/franquicia/Franquicia.java
@@ -0,0 +1,20 @@
+package co.com.franquicias.model.franquicia;
+import co.com.franquicias.model.sucursal.Sucursal;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Franquicia {
+    private Integer idFranquicia;
+    private String nombreFranquicia;
+    private List<Sucursal> listaSucursales;
+}
