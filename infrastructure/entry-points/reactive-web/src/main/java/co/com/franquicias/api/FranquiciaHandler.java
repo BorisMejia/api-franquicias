58cbd27a73f7678c9ef0e438ac46d8a000e84d78
BorisMejia <borisfernandez916@gmail.com>
2025-10-03 10:51:36 -0500

    feat: implementa configuración de CORS y seguridad en el proyecto reactive-web

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/franquicias/api/FranquiciaHandler.java
@@ -0,0 +1,13 @@
+package co.com.franquicias.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class FranquiciaHandler {
+
+}
