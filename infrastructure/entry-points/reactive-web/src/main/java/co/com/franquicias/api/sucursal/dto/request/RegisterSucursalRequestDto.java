6996a4d1c65aa9ffb4a88634ddf74da107a80ab1
BorisMejia <borisfernandez916@gmail.com>
2025-10-03 18:44:22 -0500

    feat: se añaden descripciones de esquema a los DTOs con swagger

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/franquicias/api/sucursal/dto/request/RegisterSucursalRequestDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/franquicias/api/sucursal/dto/request/RegisterSucursalRequestDto.java
@@ -1,7 +1,12 @@
 package co.com.franquicias.api.sucursal.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO para registrar una nueva sucursal por franquicia")
 public record RegisterSucursalRequestDto(
+        @Schema(description = "ID de una Franquicia Existente", example = "1", required = true)
         Integer idFranquicia,
+        @Schema(description = "Nombre descriptivo de la sucursal", example = "Sucursal Medellín", required = true)
         String nombreSucursal
 ) {
 }
