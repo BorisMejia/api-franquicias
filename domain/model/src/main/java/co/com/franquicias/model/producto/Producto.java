2e3a4af2f34dad33855431e9ab5e5aaa3d4ecf05
BorisMejia <borisfernandez916@gmail.com>
2025-10-04 15:49:10 -0500

    feat: se implementa el idSucursal en el modelo producto

--- domain/model/src/main/java/co/com/franquicias/model/producto/Producto.java
+++ domain/model/src/main/java/co/com/franquicias/model/producto/Producto.java
@@ -2,16 +2,17 @@ package co.com.franquicias.model.producto;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Producto {
     private Integer idProducto;
     private String nombreProducto;
     private Integer stock;
+    private Integer idSucursal;
 }
